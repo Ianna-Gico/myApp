87f51ab1ce64d58786db027f41de1e46ac5a06cb
ianna.gico@gmail.com <ianna.gico.silva@avanade.com>
2021-04-05 15:34:43 -0300

    ajustes users, usuarios e usuariosController

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,6 +1,8 @@
+const usuarios = require('../models/usuarios');
+
 const usuariosController = {
-    index: (req,res) =>{
-        return res.send ('estou exibindo essa mensagem através do Controller!');
+    index: (req, res) => {
+        return res.send(usuarios.listarUsuarios());
     }
 }
 module.exports = usuariosController;
